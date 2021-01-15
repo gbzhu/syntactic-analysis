65bb6eb7f7ffeedf4700a442128d8ebba3bcb4df
gbzhu <gbzhu@zju.edu.cn>
2021-01-15 10:24:55 +0800

    modify the server port from 5001 to 5000

--- syntactic_analysis.py
+++ syntactic_analysis.py
@@ -1224,4 +1224,4 @@ def main():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
-    # app.run(host='0.0.0.0', port=5001)
+    # app.run(host='0.0.0.0', port=5000)
